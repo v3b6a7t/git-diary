f2661ae4c22777589ef5b2cc71603140b74a73b6
v3b6a7t <v3b6a7t@webbart.eu>
2020-07-17 22:47:15 +0200

    Test 'required' has been modified

--- tests/required.sh
+++ tests/required.sh
@@ -9,7 +9,12 @@ require_source "config"
 # COMPONENT TESTING
 
 for DIRECTORY in $INST_DIRECTORIES; do
-
     do_existence_test $DIRECTORY
-
 done
+
+for FILE in $INST_FILES; do
+    do_existence_test $FILE
+done
+
+
+
