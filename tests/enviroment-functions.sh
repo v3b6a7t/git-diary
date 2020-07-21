a61598f3b2227792e31537270057ce349ac41d83
v3b6a7t <v3b6a7t@webbart.eu>
2020-07-21 09:48:45 +0200

    Test rebuild begins

---
+++ tests/enviroment-functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source "scripts/utils/test-tools.sh"
+source "scripts/utils/enviroment.sh"
+
+# CONFIG FUNCTION TESTING
+
+get_config_value    "GIT_VERSION"
+
+test_result "GIT_VERSION" $GIT_VERSION
+
+
+# GIT FUNCTIONS TESTING
+
+get_current_branch  "CURRENT_BRANCH"
+get_master_branch   "MASTER_BRANCH"
+
+test_result "CURRENT_BRANCH" $CURRENT_BRANCH
+test_result "MASTER_BRANCH" $MASTER_BRANCH
